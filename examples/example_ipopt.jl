76489d34382a8abc60c3f470576f9e19b28c69c8
Yuri <yuri.shimane@gmail.com>
2024-12-12 00:59:17 +0900

    Update example_ipopt.jl

--- examples/example_ipopt.jl
+++ examples/example_ipopt.jl
@@ -2,60 +2,86 @@
 Example using NLPJuMP with Ipopt
 """
 
+using GLMakie
+using Ipopt
 using JuMP
-push!(LOAD_PATH, "../src/")
-using NLPSaUT
+# push!(LOAD_PATH, "../src/")
+# using NLPSaUT
+include(joinpath(@__DIR__, "../src/NLPSaUT.jl"))
 
-NLPSaUT.test_ipopt()
 
-# """
-# User-defined fitness function
+"""
+User-defined fitness function
 
-# # Returns:
-# 	- `Vector`: vector stores [f, h, g] where f is objective, h is equality, and g is inequality constraints
-# """
-# function f_fitness(x...)    
-# 	# objective
-#     f = x[1]^2 - x[2]
+# Returns:
+	- `Vector`: vector stores [f, h, g] where f is objective, h is equality, and g is inequality constraints
+"""
+function f_fitness(x::T...) where {T<:Real}
+	# objective
+    f = x[1]^2 - x[2]
     
-#     # equality constraints
-#     h = zeros(1,)
-#     h = x[1]^3 + x[2] - 1
+    # equality constraints
+    h = zeros(T, 1)
+    h = x[1]^3 + x[2] - 2.4
 
-#     # inequality constraints
-#     g = zeros(2,)
-#     g[1] = x[1]^2 + x[2]^2 - 1
-#     g[2] = x[2] - 2
-    
-#     fitness = vcat(f,h,g)[:]
-#     return fitness
-# end
+    # inequality constraints
+    g = zeros(T, 2)
+    g[2] = -0.3x[1] + x[2] - 2   # y <= 0.3x + 2
+    g[1] = x[1] + x[2] - 5      # y <= -x + 5
+    return [f; h; g]
+end
 
 
-# # ------------------------------------------------------------------------- #
-# ## Solve model
-# # problem dimensions
-# nx = 2                   # number of decision vectors
-# nh = 1                   # number of equality constraints
-# ng = 2                   # number of inequality constraints
-# lx = -10*ones(nx,)
-# ux =  10*ones(nx,)
-# x0 = [1.2, 0.9]
+# ------------------------------------------------------------------------- #
+## Solve model
+# problem dimensions
+nx = 2                   # number of decision vectors
+nh = 1                   # number of equality constraints
+ng = 2                   # number of inequality constraints
+lx = -10*ones(nx,)
+ux =  10*ones(nx,)
+x0 = [-1.2, 10]
 
-# # get model
-# order = 2
-# diff_f = "forward"
-# x, model = build_model(f_fitness, nx, nh, ng, lx, ux, x0)#, order, diff_f)
-# set_optimizer_attribute(model, "tol", 1e-6)
-# set_optimizer_attribute(model, "print_level", 5)
-# println(model)
+# get model
+order = 2
+diff_f = "forward"
+model = Model(Ipopt.Optimizer)
+NLPSaUT.build_model!(model, f_fitness, nx, nh, ng, lx, ux, x0;)
+set_optimizer_attribute(model, "tol", 1e-12)
+set_optimizer_attribute(model, "print_level", 5)
+println(model)
 
-# # run optimizer
-# optimize!(model)
+# run optimizer
+optimize!(model)
+xopt = value.(model[:x])
 
-# # print results
-# println(termination_status(model))
-# println("Decision vector: ")
-# println(value.(x))
-# println("Objective: ")
-# println(objective_value(model))
+# checks
+@assert is_solved_and_feasible(model)
+
+# print results
+println(termination_status(model))
+println("Decision vector: ")
+println(xopt)
+println("Objective: ")
+println(objective_value(model))
+
+# plot contour
+fig = Figure(size=(500,500))
+ax = Axis(fig[1,1], xlabel="x1", ylabel="x2")
+xs_grid = LinRange(-11, 11, 100)
+ys_grid = LinRange(-11, 11, 100)
+contourf!(ax, xs_grid, ys_grid, (x, y) -> f_fitness(x, y)[1], levels=20)
+
+# plot constraints
+lines!(ax, [x for x in xs_grid], [2.4 - x^3 for x in xs_grid], color=:blue)
+fill_between!(ax, xs_grid, maximum(xs_grid) * ones(length(xs_grid)), 
+    [0.3x + 2 for x in xs_grid], color=:black, alpha=0.35)
+fill_between!(ax, xs_grid, maximum(xs_grid) * ones(length(xs_grid)), 
+    [5 - x for x in xs_grid], color=:black, alpha=0.35)
+
+# plot solution
+scatter!(ax, [value(model[:x][1])], [value(model[:x][2])], markersize=5, color=:red)
+
+xlims!(ax, minimum(xs_grid), maximum(xs_grid))
+ylims!(ax, minimum(ys_grid), maximum(ys_grid))
+display(fig)
